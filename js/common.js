4f93b9b48d72f6d4e92085efc96481e75f553bc3
dbsk1111 <dbsk1111@gmail.com>
2021-05-17 16:36:33 +0900

    210517

--- js/common.js
+++ js/common.js
@@ -1,17 +1,54 @@
 $(function(){
-  $(window).scroll(function(){
-    let winHeight = $(window).height();
-    let scTop = $(this).scrollTop();
-    let headerFix = $("header");
+  // 선언
+  let docHeight = $(document).height();
+  let winHeight = $(window).height();
+  let scTop = $(document).scrollTop();
+  let fixClass = $("header");
 
+  // 초기화
+  let barInitial = (scTop/(docHeight-winHeight))*100;
+  $('.progress_bar').css('width',barInitial+'%')
+  headerFix();
 
-    console.log(winHeight);
-    console.log(scTop);
+  $('nav li').click(function(){
+    let navId = $(this).text();
 
-    if( scTop > winHeight ){
-      headerFix.addClass("fixed");
-    }else{
-      headerFix.removeClass("fixed");
+    $('section').each(function(){
+      let secId = $(this).attr('id');
+      let secTop = $(this).offset().top;
+      if( navId.toLowerCase() == secId ){
+        window.scroll({ top: secTop, behavior: 'smooth' });
+      }
+    })
+  })
+
+  // 홈 클릭 시 스크롤 이동
+  $('#home').on('click',function(){
+    if(!$('html').is(':animated')){
+      $('html').animate( { scrollTop: $('#about').offset().top, behavior: 'smooth' },400,'swing',function(){
+      // $('html, body').animate( { scrollTop: $('#about').offset().top, behavior: 'smooth' },400,'swing',function(){
+        // event.preventDefault();
+      })
     }
-  });
+  })
+
+  // 스크롤 진행도
+  $(window).scroll(function(){
+    docHeight = $(document).height();
+    winHeight = $(window).height();
+    scTop = $(this).scrollTop();
+    headerFix();
+    // console.log('진행도: '+(scTop/(docHeight-winHeight))*100+'%')
+    let bar = (scTop/(docHeight-winHeight))*101.25;
+    $('.progress_bar').css('width', bar+'%')
+  })
+
+  // 헤더 고정
+  function headerFix(){
+    if( scTop >= winHeight ){
+      fixClass.addClass("fixed");
+    }else{
+      fixClass.removeClass("fixed");
+    }
+  }
  });
